d867534d26281064aee4ffd91eb927513d3f1856
nayem9b <razwanniam1@gmail.com>
2023-07-05 17:18:29 +0600

    store and reducer set up

---
+++ src/redux/features/counter/counterSlice.tsx
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface ICounterState {
+  count: number;
+}
+
+const initialState: ICounterState = {
+  count: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count = state.count + 1;
+    },
+    decrement: (state) => {
+      state.count = state.count - 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.count += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const counterReducer = counterSlice.reducer;
